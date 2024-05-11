048c9c1e0aa2495f1426b54ee067dc3e0c91dc51
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-05-11 14:10:43 +0300

    //Köşegenleri ve köşegenler arasındaki açı bilinen dörtgenin alanını  hesaplama

---
+++ dörtgenin_alan_hesap.cs
@@ -0,0 +1,17 @@
+//Köşegenleri ve köşegenler arasındaki açı bilinen dörtgenin alanını  hesaplama
+using System;
+class Program
+{
+   static void Main()
+    {
+        Console.WriteLine("Lütfen üçgenin 1.köşegeni giriniz:");
+        string ilk_uzunluk = Console.ReadLine();
+        Console.WriteLine("Lütfen üçgenin 2.köşegeni giriniz:");
+        string ikinci_uzunluk = Console.ReadLine();
+        Console.WriteLine("Lütfen köşegenler arasında kalan açıyı giriniz: ");
+        string acı = Console.ReadLine();
+        double alan = int.Parse(ilk_uzunluk)*int.Parse(ikinci_uzunluk)*Math.Sin(double.Parse(acı)*Math.PI/180)/2;
+        Console.WriteLine("Giridiğiniz dörtgenin alanı: {0}",alan);
+        Console.ReadLine();
+    }
+}
