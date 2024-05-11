65e9ddf3c8f7cbef7b1fbf3d47ead343efbe3a09
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-05-11 18:20:16 +0300

    Viette Formulleri

--- Viette Formulleri.cs
+++ Viette Formulleri.cs
@@ -1,1 +1,35 @@
+//Viette Formülleri
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x1, x2, ktop, kcarp;
+        Console.WriteLine("1.Kök: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("2.Kök: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        ktop = x1 + x2;
+        kcarp = x1 * x2;
+        if( ktop < 0)
+        {
+            Console.WriteLine("\nx^2+{0}x",(-1)*ktop);
+        }
+        else
+        {
+            Console.WriteLine("\n x^2-{0}x", ktop);
+        }
+        if( kcarp < 0)
+        {
+            Console.WriteLine("Değer: {0}", kcarp);
+        }
+        else
+        {
+            Console.WriteLine("+{0}",kcarp);
+        }
+
+        Console.ReadLine();
+    }
+}
 
