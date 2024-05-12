a9ddeff3be9ef1828b0cb7037522a1d02158c04d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-05-12 14:11:58 +0300

    Akrep ile Yelkovan arasında kalan açıyı hesaplama

--- Akrep ile yelkovan arasinda kalan açiyi hesaplama.cs
+++ Akrep ile yelkovan arasinda kalan açiyi hesaplama.cs
@@ -1,1 +1,27 @@
+//Akrep ile Yelkovan arasında kalan açıyı hesaplama
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen saati giriniz: ");
+        int saat = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Lütfen dakikayı giriniz: ");
+        int dakika = Convert.ToInt16(Console.ReadLine());
+        int saatdeger = 30 * saat;
+        int dakikadeger = 6 * dakika;
+        if(saatdeger > dakikadeger)
+        {
+            Console.WriteLine("{0}", saatdeger - dakikadeger);
+        }
+        else
+        {
+            Console.WriteLine("{0}", dakikadeger - saatdeger);
+
+        }
+
+
+        Console.ReadLine();
+    }
+}
 
