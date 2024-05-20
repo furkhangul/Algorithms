84ad7bcdebea9cbb1b183796710c553eec8e599d
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-05-20 20:43:10 +0300

    Maclaurin serilerinin hesaplanması

---
+++ Maclaurin serisi.cs
@@ -0,0 +1,38 @@
+//Maclaurin Serileri ile e^x hesaplama
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("x değerini girin: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("N değerini girin: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        double result = MaclaurinSeries(x, N);
+        Console.WriteLine("e^x ≈ " + result);
+
+        Console.ReadLine();
+    }
+
+    static double MaclaurinSeries(double x, int N)
+    {
+        double sum = 1.0; // Başlangıç değeri e^0 = 1
+
+        for (int i = 1; i <= N; i++)
+        {
+            sum += Math.Pow(x, i) / Factorial(i);
+        }
+
+        return sum;
+    }
+
+    static int Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Factorial(n - 1);
+    }
+}
